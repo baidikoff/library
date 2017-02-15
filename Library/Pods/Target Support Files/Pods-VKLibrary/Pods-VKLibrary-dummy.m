9ec19ca45ff3e57ce38976df50ae0e43eb38ffb6
Nick Baidikoff <nbaidikoff@gmail.com>
2017-02-15 08:53:09 +0200

    Renamed application according to App Store rules

--- Library/Pods/Target Support Files/Pods-VKLibrary/Pods-VKLibrary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VKLibrary : NSObject
-@end
-@implementation PodsDummy_Pods_VKLibrary
-@end
--- Library/Pods/Target Support Files/Pods-VKLibrary/Pods-VKLibrary-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_VKLibraryVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_VKLibraryVersionString[];
-
